83aa4214a18ab98b77fa4971624afec81d127565
Aqarius90 <aqarius90+git@gmail.com>
2016-11-30 22:49:01 +0100

    'hopeless attempts' methodology, pt1

--- js/FullDB.js
+++ js/FullDB.js
@@ -1,6 +1,11 @@
 function initDB(){
-  jQuery.get('https://aqarius90.github.io/FA_WG_Ulilities/final_data.csv', function(data) {
-    console.log(data);});
+    var DBstring;
+    var DBarr;
     ractiveDB.set({DBisNotLoaded: false});
+    jQuery.get('https://aqarius90.github.io/FA_WG_Ulilities/final_data.csv', function(data) {
+      DBstring = new String(data);
+      DBarr = jQuery.csv.toArrays(data);
+    });
+    console.log(DBarr);
     ractiveDB.set({DBisLoaded: true});
 }
