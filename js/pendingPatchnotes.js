1db020dfa26d7373fa862ae8664c3ce2b6a989d9
Unknown <aqarius90@gmail.com>
2020-11-12 19:38:45 +0100

    house cleaning

--- js/pendingPatchnotes.js
+++
@@ -1,7 +0,0 @@
-THS Mi-24A has the ground based NSVT
-Automat M-85 is a cross between an SMG and an Assault rifle
-Komandno odeljenje [cqc][stat]
-all malyutkas are now saclos
-sissi dragunov has sstabs
-M/39, all of them, are militia
-BMP-1K KOPA grom is [stat] with stabs
